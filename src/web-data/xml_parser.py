b9e1cc7ba1e5dcc08941340d82a5bf40a3b7b2e4
Vinicius Rodrigues Giannaccini <144853455+VGiannac@users.noreply.github.com>
2023-11-20 11:40:03 -0500

    Update xml_parser.py

--- src/web-data/xml_parser.py
+++ src/web-data/xml_parser.py
@@ -1,12 +1,13 @@
 # xml_parser.py
 import requests
 import xml.etree.ElementTree as ET
+import pandas as pd
 
 class XmlParser:
     def __init__(self, robots_url):
         self.robots_url = robots_url
 
-    def get_sitemap_urls(self):
+    def get_sitemap_data(self):
         # Fetch robots.txt content
         response = requests.get(self.robots_url)
         robots_content = response.text
@@ -17,4 +18,7 @@ class XmlParser:
             if line.startswith('Sitemap:'):
                 sitemap_urls.append(line.split(': ')[1].strip())
 
-        return sitemap_urls
+        # Create a DataFrame with the sitemap URLs
+        df = pd.DataFrame({'Sitemap URL': sitemap_urls})
+        return df
+
