4d982c12d113aacaf1c2a68b267d481e0c4381e3
Vinicius Rodrigues Giannaccini <144853455+VGiannac@users.noreply.github.com>
2023-11-21 13:56:11 -0500

    Update xml_parser.py
    Adding Doc. strings

--- src/web-data/xml_parser.py
+++ src/web-data/xml_parser.py
@@ -5,10 +5,28 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 
 class XmlParser:
+"""
+    A class for parsing sitemap URLs from robots.txt and storing them in a DataFrame.
+
+    Attributes:
+    - robots_url (str): The URL of the robots.txt file containing sitemap information.
+    """
     def __init__(self, robots_url):
+    """
+        Initialize the XmlParser with the URL of the robots.txt file.
+
+        Parameters:
+        - robots_url (str): The URL of the robots.txt file.
+        """
         self.robots_url = robots_url
 
     def get_sitemap_urls(self):
+    """
+        Retrieve sitemap URLs from the robots.txt file.
+
+        Returns:
+        - list: A list of sitemap URLs.
+        """
         # Get content from robots.txt
         response = requests.get(self.robots_url)
         robots_content = response.text
@@ -22,6 +40,12 @@ class XmlParser:
         return sitemap_urls
 
     def get_sitemap_data(self):
+    """
+        Retrieve and store sitemap URLs in a DataFrame.
+
+        Returns:
+        - pd.DataFrame: A DataFrame containing sitemap URLs.
+        """
         # Get sitemap URLs
         sitemap_urls = self.get_sitemap_urls()
 
